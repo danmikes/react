ef5f15a088fb366bd7f69275aa3a8bfb131f0027
Daniel Mikes <max8@post.cz>
2025-01-16 11:58:59 +0100

    update

--- src/page/AboutPage.jsx
+++ src/page/AboutPage.jsx
@@ -1,3 +1,4 @@
+import React from 'react'
 import Title from '../component/Title'
 import DetailSection from '../component/DetailSection'
 import SkillSection from '../component/SkillSection'
@@ -15,18 +16,28 @@ function AboutPage() {
 
       <Title title={'skills'} span={'skills'} />
       <div className="skills-container">
-        <SkillSection skill={'Java/SpringBoot'} progress={'80%'} />
-        <SkillSection skill={'TypeScript/Angular'} progress={'60%'} />
+        <h1>Code</h1>
         <SkillSection skill={'JavaScript/ReactJS'} progress={'40%'} />
         <SkillSection skill={'JavaScript/ReactNative'} progress={'10%'} />
-        <SkillSection skill={'Python/Flask'} progress={'70%'} />
-        <SkillSection skill={'Vi/Bash/LateX'} progress={'80%'} />
-        <SkillSection skill={'SQL/PostgreSql'} progress={'50%'} />
-        <SkillSection skill={'noSQL/MongoDB'} progress={'40%'} />
-        <SkillSection skill={'GraphQL/GraphQLPlayground'} progress={'10%'} />
-        <SkillSection skill={'LinkedData/SparQL'} progress={'5%'} />
-        <SkillSection skill={'Git/GitHub/GitLab'} progress={'90%'} />
+        <SkillSection skill={'Java/SpringBoot'} progress={'80%'} />
         <SkillSection skill={'Machine Learning/AI'} progress={'10%'} />
+        <SkillSection skill={'Python/Flask'} progress={'70%'} />
+        <SkillSection skill={'TypeScript/Angular'} progress={'60%'} />
+        <SkillSection skill={'Vi/Bash/LateX'} progress={'90%'} />
+      </div>
+
+      <div className="skills-container">
+        <h1>Data</h1>
+        <SkillSection skill={'LinkedData/SparQL'} progress={'5%'} />
+        <SkillSection skill={'noSQL/MongoDB'} progress={'40%'} />
+        <SkillSection skill={'SQL/PostgreSql'} progress={'50%'} />
+      </div>
+
+      <div className="skills-container">
+        <h1>Infra</h1>
+        <SkillSection skill={'Git/GitHub/GitLab'} progress={'90%'} />
+        <SkillSection skill={'GraphQL/GraphQLPlayground'} progress={'10%'} />
+        <SkillSection skill={'AWS/Terraform'} progress={'90%'} />
       </div>
 
       <Title title={'services'} span={'service'} />
