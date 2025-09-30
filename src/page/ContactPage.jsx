2f1e364355efdbb583bcee2c3ebe91d2ff945375
Daniel Mikes <max8@post.cz>
2025-09-30 14:55:24 +0200

    map

--- src/page/ContactPage.jsx
+++ src/page/ContactPage.jsx
@@ -11,12 +11,13 @@ function ContactPage() {
       <div className="contact-details">
         <div className="map-sect">
           <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d24286.645847275613!2d4.639997370900829!3d52.29140186109729!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c5e7afa59ae60d%3A0x3f11830627093e83!2sHoofddorp!5e0!3m2!1sen!2snl!4v1623790131646!5m2!1sen!2snl"
+            src="https://maps.google.com/maps?q=Ina%20Boudier-Bakkerdreef%2C%20Hoofddorp%2C%20Netherlands&t=&z=13&ie=UTF8&iwloc=&output=embed"
             width="600"
             height="450"
             style={{ border: 0 }}
             allowFullScreen=""
             loading="lazy"
+            referrerpolicy="no-referrer-when-downgrade"
             title="Map of location"
           ></iframe>
         </div>
